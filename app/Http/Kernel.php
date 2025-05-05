20bed0bb194c7fadff04796e1bc49b18e5383502
Martin Alonso <martinnets@hotmail.com>
2025-05-05 17:08:12 -0500

    v0.0.23

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,10 +6,10 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
-    // protected $middleware = [
-    //     // otros middlewares...
-    //     \App\Http\Middleware\RemoveXFrameOptions::class,
-    // ];
+    protected $middleware = [
+        // otros middlewares...
+        \App\Http\Middleware\Cors::class,
+    ];
     // protected $routeMiddleware = [
     //     // otros middlewares...
     //     'no-frame-header' => \App\Http\Middleware\RemoveXFrameOptions::class,
